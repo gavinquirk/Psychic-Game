ca90b7e5300b3517bb0c7c9008c145685d40e475
Gavin Quirk <gavinquirk@gmail.com>
2018-02-02 11:51:00 -0800

    updated javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,11 +3,12 @@ var lossCounter = 0
 var guessCounter = 9
 var playerGuess = 0
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var wins = document.getElementById("wins")
 var compChoice = ""
 
 // BEGINNING OF DOCUMENT READY
 $(document).ready(function() {
+
+    var compChoice = ""
 
     // Create screen text
     document.querySelector("#wins").textContent = "Wins:  " + winCounter
@@ -15,15 +16,14 @@ $(document).ready(function() {
     document.querySelector("#guessChoices").textContent = "Guess Choices:  " + guessChoices
     document.querySelector("#guessesLeft").textContent = "Guesses Left:  " + guessCounter
 
-    // Computer Choice Logic
-        
+
+    // Computer Choice Logic Function
     function compLogic() {
-        var compChoice = alphabet[Math.floor(Math.random() * 10)]
+        var compChoice = alphabet[Math.floor(Math.random() * 26)]
+        console.log(compChoice)
     }
 
     compLogic()
-    console.log(compChoice)
-
 
 
     // On key up, run a function
@@ -33,20 +33,23 @@ $(document).ready(function() {
 
         // put the pressed key into a variable
         var playerChoice = event.key
+        var compChoice = compChoice
         console.log("Choice: " + playerChoice)
         
-        // Reset Conditions
-        if (guessCounter === 0) {
-            guessCounter = 9
-            alert("You have lost! Game resetting...")
-        }
         // Win Conditions
-        else if (playerChoice === compChoice) {
+        if (playerChoice === compChoice) {
             winCounter++
+            complogic()
+            console.log(compChoice)
+            alert("You have won! Game resetting...")
         }
         // Loss Conditions
-        else if (playerChoice != compChoice) {
+        else if (guessCounter === 0) {
+            guessCounter = 9
             lossCounter++
+            compLogic()
+            console.log(compChoice)
+            alert("You have lost! Game resetting...")
         }
         
 
